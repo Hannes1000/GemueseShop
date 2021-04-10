f44e915513f388d1e910cb5d7ca71f4040d2c678
Hannes1000 <60342304+Hannes1000@users.noreply.github.com>
2021-04-10 18:56:25 +0200

    Updated UI

--- client/src/components/views/ProductViewPage/ProductViewPage.js
+++ client/src/components/views/ProductViewPage/ProductViewPage.js
@@ -5,6 +5,7 @@ import { Card, Col, Row, Input, Button, Switch} from 'antd';
 import Popup from 'reactjs-popup';
 import ImageSlider from "../../utils/ImageSlider"
 import CheckboxFilter from './Sections/CheckboxFilter';
+import { set } from 'mongoose';
 const {Meta} = Card;
 
 function ProductViewPage(props) {
@@ -13,6 +14,8 @@ function ProductViewPage(props) {
     const [availableValue, setAvailableValue] = useState(true)
     const [selectedValue, setSelectedValue] = useState([])
     const [amountValue, setAmountValue] = useState([])
+    const [amountWhich, setAmountWhich] = useState([])
+
 
     useEffect(() =>{
         //console.log(props)
@@ -31,6 +34,7 @@ function ProductViewPage(props) {
                     //console.log(product.available)
                     setSelectedValue(oldArray => [...oldArray, false])
                     setAmountValue(oldArray => [...oldArray, "0"])
+                    setAmountWhich(oldArray => [...oldArray, {which: "kg"}])
                 });
             }else{
                 alert("Failed to fetch product data")
@@ -53,27 +57,58 @@ function ProductViewPage(props) {
     }
 
     const onSelectedSwitch = (index) =>{
+
+        var element = document.getElementById(index);
+        
+        //console.log(selectedValue[index])
         //console.log(selectedValue)
         //console.log(index)
+        //console.log("switched selecetd")
         let arr = selectedValue;
         var start_index = index
         var number_of_elements_to_remove = 1;
         //console.log(arr[index])
         var replacement;
+        
         if(arr[index] == true){
             replacement = false;
+            element.style.backgroundColor = "rgba(38, 125, 25, 0.329)"
         }else{
             replacement = true;
+            element.style.backgroundColor = "rgba(68, 212, 45, 0.8)"
         }
         arr.splice(start_index, number_of_elements_to_remove, replacement);
         //console.log(arr)
         setSelectedValue(arr)
-        // let arr = []
-        // arr = selectedValue
-        // console.log(arr)
-        // arr.splice(index, 1, !arr.indexOf(index))
-        // console.log(arr.indexOf(index))
-        // setSelectedValue(arr)
+    }
+
+    const onSelectedSwitch2 = (event, index) =>{
+        //console.log(event.target.className)
+        if (event.target.className === "ant-input input-amount" || event.target.className === "select-which" )
+        return;
+
+        var element = document.getElementById(index);
+        
+        //console.log(selectedValue[index])
+        //console.log(selectedValue)
+        //console.log(index)
+        //console.log("switched selecetd")
+        let arr = selectedValue;
+        var start_index = index
+        var number_of_elements_to_remove = 1;
+        //console.log(arr[index])
+        var replacement;
+        
+        if(arr[index] == true){
+            replacement = false;
+            element.style.backgroundColor = "rgba(38, 125, 25, 0.329)"
+        }else{
+            replacement = true;
+            element.style.backgroundColor = "rgba(68, 212, 45, 0.8)"
+        }
+        arr.splice(start_index, number_of_elements_to_remove, replacement);
+        //console.log(arr)
+        setSelectedValue(arr)
     }
 
     const onOrderClick = ()=>{
@@ -85,12 +120,15 @@ function ProductViewPage(props) {
             if(selectedValue[i] == true && amountValue[i] != "0" && amountValue[i] != ""){
                 //arrSelectedProducts.push(products[i]._id)
                 arrSelectedProducts.push(products[i])
-                arrAmount.push(amountValue[i])
+                let amountstr = amountValue[i]
+                amountstr += " " + amountWhich[i].which
+                //console.log(amountstr)
+                arrAmount.push(amountstr)
             }
         }
 
-        console.log(arrSelectedProducts)
-        console.log(arrAmount)
+        //console.log(arrSelectedProducts)
+        //console.log(arrAmount)
 
         const variables ={
             user: props.user.userData._id,
@@ -109,9 +147,9 @@ function ProductViewPage(props) {
     }
 
     const onChangeAmount = (event, index)=>{
-        // console.log(index)
         // console.log(event.currentTarget.value)
         // console.log(amountValue)
+        //console.log("changed amount")
         let arr = amountValue;
         var start_index = index
         var number_of_elements_to_remove = 1;
@@ -125,17 +163,39 @@ function ProductViewPage(props) {
         arr.splice(start_index, number_of_elements_to_remove, event.currentTarget.value);
         //console.log(arr)
         setAmountValue(arr)
+
+        //switch selected
+        let arrSelected = selectedValue;
+        
+        var selected;
+        if(arrSelected[index] == false || event.currentTarget.value === ""){
+            onSelectedSwitch(index)
+        }
     }
 
+    const onChangeWhich =(e, index) => {
+        let arr = amountWhich;
+        var start_index = index
+        var number_of_elements_to_remove = 1;
+        //console.log(arr[index])
+        arr.splice(start_index, number_of_elements_to_remove, {which: e.currentTarget.value});
+        //console.log(arr)
+        setAmountWhich(arr)
+    }
 
     //lg = largeSize; md = mediumSize; xs = smallSize
     const renderCards = products.map((product, index)=>{
 
-        return <Button className="productcard" key={index}>
+        return <Button 
+            className="productcard" 
+            key={index}
+            onClick={(event)=> onSelectedSwitch2(event, index)}
+            id={index}
+            >
                     <div id="productname">
                         {product.name}
                     </div>
-                    <Switch style={{position:"absolute", right:"10px", bottom: "10px"}} defaultChecked={(selectedValue[index] == true)} onChange={()=> onSelectedSwitch(index)} />
+                    {/* <Switch style={{position:"absolute", right:"10px", bottom: "10px"}} defaultChecked={(selectedValue[index] == true)} onChange={()=> onSelectedSwitch(index)} /> */}
                     <div id="description">
                         {product.description}
                     </div>
@@ -145,9 +205,21 @@ function ProductViewPage(props) {
                     <Input
                         placeholder={"Menge Angeben"}
                         onChange={(event)=>onChangeAmount(event, index)}
-                        style={{position:"absolute", right: "0px", top: "300px"}}
+                        style={{position:"absolute", right: "75px", top: "298px"}}
+                        className="input-amount"
+                        type="number"
                     >
                     </Input>
+                    <select 
+                        style={{position:"absolute", right: "0px", top: "298px"}}
+                        className="select-which"
+                        // value={amountWhich[index].which ? "kg" : amountWhich[index].which}
+                        onChange={(e) => onChangeWhich(e, index)}
+                        >
+                        <option name="kg">Kg</option>
+                        <option name="stk">Stück</option>
+                        <option name="g">Gramm</option>
+                    </select>
                 </Button>
             {/* <Card hoverable={true} cover={<ImageSlider images={product.images} />} bordered={true} >
                 <Meta title={product.name} id="productname">
